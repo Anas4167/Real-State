eadcdcd49593944034259191183caf14dc366ceb
Anas <anascabdi199@gmail.com>
2025-09-06 08:56:33 +0930

    last edit

--- main.js
+++ main.js
@@ -19,10 +19,26 @@ const btnPentHouse=document.querySelector("#btnPentHouse")
 const apartment=document.querySelector("#apartment")
 const villa=document.querySelector("#villa")
 const pentHouse=document.querySelector("#pentHouse")
+const bestProDet=document.querySelector("#bestProDet")
+const getProDet=document.querySelector("#getProDet")
+const howProDet=document.querySelector("#howProDet")
+const dolorProDet=document.querySelector("#dolorProDet")
+const whyProDet=document.querySelector("#whyProDet")
+const almistProDet=document.querySelector("#almistProDet")
+const btnApartmentDet=document.querySelector("#btnApartmentDet")
+const btnVillaDet=document.querySelector("#btnVillaDet")
+const btnPentHouseDet=document.querySelector("#btnPentHouseDet")
+const apartmentDet=document.querySelector("#apartmentDet")
+const villaDet=document.querySelector("#villaDet")
+const pentHouseDet=document.querySelector("#pentHouseDet")
+
+console.log(btnApartmentDet, btnVillaDet, btnPentHouseDet, apartmentDet, villaDet, pentHouseDet)
+
+console.log(bestProDet, getProDet, whyProDet, dolorProDet, howProDet,almistProDet)
+console.log(bestInHome, getInHome, howInHome, whyInHome, dolorInHome, almistInHome)
 
 
-
-
+//  section one
 angleRight1.addEventListener("click", ()=> {
     kan1aad.style.display = "none"
     kan2aad.style.display = "block"
@@ -61,6 +77,8 @@ angleLeft3.addEventListener("click", ()=> {
 })
 
 
+
+// section two
 bestInHome.addEventListener("click", ()=>{
     getInHome.style.display = getInHome.style.display === "block" ? "none" : "block"
     bestInHome.style.color = getInHome.style.display === "block" ? "orangered" : "black"
@@ -90,25 +108,80 @@ whyInHome.addEventListener("click", ()=>{
 
 })
 
+
+// section four
 btnVilla.addEventListener("click", ()=>{
     villa.style.display="block"
+    btnVilla.style.backgroundColor="orangered"
     apartment.style.display="none"
+    btnApartment.style.backgroundColor="black"
     pentHouse.style.display="none"
-
+    btnPentHouse.style.backgroundColor="black"
+    villa.classList.remove("animate-scaleUp") 
+    void villa.offsetWidth; 
+    villa.classList.add("animate-scaleUp")
 })
 
 
 btnApartment.addEventListener("click", ()=>{
     villa.style.display="none"
+    btnVilla.style.backgroundColor="black"
     apartment.style.display="block"
+    btnApartment.style.backgroundColor="orangered"
     pentHouse.style.display="none"
+    btnPentHouse.style.backgroundColor="black"
 
 })
 
 
 btnPentHouse.addEventListener("click", ()=>{
     villa.style.display="none"
+    btnVilla.style.backgroundColor="black"
     apartment.style.display="none"
+    btnApartment.style.backgroundColor="black"
     pentHouse.style.display="block"
+    btnPentHouse.style.backgroundColor="orangered"
 
 })
+
+
+
+// propertyDetail paragraphs
+// bestProDet.addEventListener("click", ()=>{
+//     getProDet.style.display = getProDet.style.display === "block" ? "none" : "block"
+//     bestProDet.style.color = getProDet.style.display === "block" ? "orangered" : "black"
+//     dolorProDet.style.display= "none"
+//     almistProDet.style.display= "none"
+//     howProDet.style.color = "black"
+//     whyProDet.style.color = "black"
+// })
+
+// howProDet.addEventListener("click", ()=>{
+//     getProDet.style.display= "none"
+//     dolorProDet.style.display= dolorProDet.style.display=== "block" ? "none" : "block"
+//     howProDet.style.color = dolorProDet.style.display === "block" ? "orangered" : "black"
+//     almistProDet.style.display= "none"
+//     bestProDet.style.color = "black"
+//     whyProDet.style.color = "black"
+// })
+
+// whyProDet.addEventListener("click", ()=>{
+//     getProDet.style.display= "none"
+//     dolorProDet.style.display= "none"
+//     almistProDet.style.display= almistProDet.style.display=== "block" ? "none" : "block"
+//     whyProDet.style.color = almistProDet.style.display === "block" ? "orangered" : "black"
+//     bestProDet.style.color = "black"
+//     howProDet.style.color = "black"
+// })
+
+// property detail buttons
+
+btnVillaDet.addEventListener("click", () =>{
+    villaDet.style.display="block"
+    apartmentDet.style.display="none"
+    pentHouseDet.style.display="none"
+})
+
+
+
+
